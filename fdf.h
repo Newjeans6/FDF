e1dbee96ecd97aebc58682329919931b6e9910c2
Pierrick Naessens <pierrick.naessens62@gmail.com>
2024-12-03 16:03:53 +0100

    leaks and image

--- fdf.h
+++ fdf.h
@@ -6,25 +6,41 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 11:48:31 by pnaessen          #+#    #+#             */
-/*   Updated: 2024/12/02 16:36:55 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2024/12/03 16:03:10 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef fdf_h
-# define fdf_h
+#ifndef FDF_H
+# define FDF_H
 
-#include "minilibx-linux/mlx.h"
-#include <stddef.h>
+# include "minilibx-linux/mlx.h"
+//# include <./X11/Keysym.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-
-#define MLX_ERROR 1
-#define ESC_KEY 27
-#define DESTROY 17  
+# define MLX_ERROR 1
+# define ESC_KEY 65307
+# define DESTROY 17
+# define KeyPress 02
 
 typedef struct s_data
 {
-    void    *mlx_ptr;
-    void    *win_ptr;
-} t_data;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+	char	*addr_ptr;
+	int		bits_per_pixel;
+	int		line_lenght;
+	int		endian;
+}			t_data;
+
+typedef struct img_s
+{
+	void	*img;
+	char	*./pnaessen.xpm;
+	int		img_width;
+	int		img_height;
+}			img_t;
 
 #endif
--- main.c
+++ main.c
@@ -6,42 +6,49 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 10:30:13 by pnaessen          #+#    #+#             */
-/*   Updated: 2024/12/02 16:39:58 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2024/12/03 16:03:24 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int key_hook(int keycode, t_data *data)
+int	key_hook(int keycode, t_data *data)
 {
-    if (keycode == ESC_KEY)
-    {
-        mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-        exit(0);
-    }
-    return (0);
+	if (keycode == ESC_KEY)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		exit(0);
+	}
+	printf("Key code : %d\n", keycode);
+	return (0);
 }
 
-int close_window(t_data *data)
+int	close_window(t_data *data)
 {
-    mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-    exit(0);
-    return (0);
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	exit(0);
+	return (0);
 }
-int main(void)
+int	main(void)
 {
-    t_data data;
+	t_data	data;
+	img_t	img;
 
-    data.mlx_ptr = mlx_init();
-    if (data.mlx_ptr == NULL)
-        return (MLX_ERROR);
-    data.win_ptr = mlx_new_window(data.mlx_ptr, 1080, 920, "First window !");
-    if (data.win_ptr == NULL)
-        return (MLX_ERROR);
-
-    mlx_key_hook(data.win_ptr, key_hook, &data); // avec echap
-    mlx_hook(data.win_ptr, DESTROY_NOTIFY, 0, close_window, &data); // ferme avec croix
-    mlx_loop(data.mlx_ptr);
-
-    return (0);
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (free(data.mlx_ptr), MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 1080, 920, "First window !");
+	if (data.win_ptr == NULL)
+		return (MLX_ERROR);
+	img.img = mlx_xpm_file_to_image(data.mlx_ptr, img.relative_path, &img.img_width,
+			&img.img_height);
+	data.img_ptr = mlx_new_image(data.mlx_ptr, 1080, 920);
+	data.addr_ptr = mlx_get_data_addr(data.img_ptr, &data.bits_per_pixel,
+			&data.line_lenght, &data.endian);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img_ptr, 0, 0);
+	mlx_key_hook(data.win_ptr, key_hook, &data);
+	mlx_hook(data.win_ptr, DESTROY, 0, close_window, &data);
+	mlx_hook(data.win_ptr, KeyPress, 0, key_hook, &data);
+	mlx_loop(data.mlx_ptr);
+	return (0);
 }
