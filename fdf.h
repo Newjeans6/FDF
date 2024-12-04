40d3765d18c89002bff145ae2fa7251a9cb4953e
Pierrick Naessens <pierrick.naessens62@gmail.com>
2024-12-04 12:49:53 +0100

    Mo leaks put picture

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 11:48:31 by pnaessen          #+#    #+#             */
-/*   Updated: 2024/12/03 16:11:22 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2024/12/04 11:26:16 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,10 @@ typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	void	*img_ptr;
-	char	*addr_ptr;
-	int		bits_per_pixel;
-	int		line_lenght;
-	int		endian;
-}			t_data;
-
-typedef struct img_s
-{
 	void	*img;
 	char	*img_path;
 	int		img_width;
 	int		img_height;
-}			img_t;
+}			t_data;
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 10:30:13 by pnaessen          #+#    #+#             */
-/*   Updated: 2024/12/03 16:11:29 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2024/12/04 12:48:01 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int	key_hook(int keycode, t_data *data)
 {
 	if (keycode == ESC_KEY)
 	{
-        mlx_destroy_image(data->mlx_ptr, data->img_ptr);
-        mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-        mlx_destroy_display(data->mlx_ptr);
-        free(data->mlx_ptr);    
+		mlx_destroy_image(data->mlx_ptr, data->img);
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		mlx_destroy_display(data->mlx_ptr);
+		free(data->mlx_ptr);
 		exit(0);
 	}
 	printf("Key code : %d\n", keycode);
@@ -28,37 +28,33 @@ int	key_hook(int keycode, t_data *data)
 
 int	close_window(t_data *data)
 {
-    mlx_destroy_image(data->mlx_ptr, data->img_ptr);
-    mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-    mlx_destroy_display(data->mlx_ptr);
-    free(data->mlx_ptr); 
-    exit(0);
+	mlx_destroy_image(data->mlx_ptr, data->img);
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+	exit(0);
 	return (0);
 }
+
 int	main(void)
 {
 	t_data	data;
-	img_t	img;
 
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 		return (free(data.mlx_ptr), MLX_ERROR);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 1080, 920, "First window !");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 1920, 1080, "First window !");
 	if (data.win_ptr == NULL)
 		return (MLX_ERROR);
-    img.img_path = "pnaessen.xpm";
-	img.img = mlx_xpm_file_to_image(data.mlx_ptr, img.img_path, &img.img_width,
-			&img.img_height);
-    if (img.img == NULL)
-         return (free(data.mlx_ptr), MLX_ERROR);    
-	//data.img_ptr = mlx_new_image(data.mlx_ptr, 1080, 920);
-	//data.addr_ptr = mlx_get_data_addr(data.img_ptr, &data.bits_per_pixel,
-	//		&data.line_lenght, &data.endian);
-	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img_ptr, 0, 0);
+	data.img_path = "pnaessen.xpm";
+	data.img = mlx_xpm_file_to_image(data.mlx_ptr, data.img_path,
+			&data.img_width, &data.img_height);
+	if (data.img == NULL)
+		return (free(data.mlx_ptr), MLX_ERROR);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img, 0, 0);
 	mlx_key_hook(data.win_ptr, key_hook, &data);
 	mlx_hook(data.win_ptr, DESTROY, 0, close_window, &data);
-	mlx_hook(data.win_ptr, KeyPress, 0, key_hook, &data);
 	mlx_loop(data.mlx_ptr);
-    free(data.mlx_ptr); 
+	free(data.mlx_ptr);
 	return (0);
 }
